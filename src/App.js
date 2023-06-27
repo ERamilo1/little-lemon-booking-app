58e7369860ba26cba6db809394b9996868e51713
Edwin Ramilo <ramilo.edwin.business@gmail.com>
2023-06-27 18:32:40 -0400

    Used useReducer hook to control state of available times in App component. Passed down state to Booking Form. Updated onChange attribute of date input.

--- src/App.js
+++ src/App.js
@@ -6,15 +6,30 @@ import Login from './Login';
 import Online from './Online';
 import Reservations from "./Reservations"
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import BookingForm from './BookingForm';
+import { useState, useReducer } from 'react';
 
 function App() {
+  const updateTimes = (state, action) => {
+    return state;
+  }
+
+  const initializeTimes = () => {
+    const initialState = ["15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+    return initialState;
+  }
+
+  const initialState2 = ["15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
+  const [state, dispatch] = useReducer(updateTimes, initializeTimes());
+
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />}/>
           <Route path="menu" element={<Menu />}/>
-          <Route path="reservations" element={<Reservations />}/>
+          <Route path="reservations" element={<BookingForm avTime={state} dispatch={dispatch}/>}/>
           <Route path="about" element={<About />}/>
           <Route path="login" element={<Login />}/>
           <Route path="onlineOrder" element={<Online />}/>
--- src/BookingForm.js
+++ src/BookingForm.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-function BookingForm() {
-    const [availableTimes, setAvailableTimes] = useState(["15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]);
+function BookingForm(props) {
     const [time, setTime] = useState("");
     const [date, setDate] = useState("");
     const [guests, setGuests] = useState(1);
     const [occasion, setOccasion] = useState("");
+    const availableTimes = props.avTime;
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -13,23 +13,26 @@ function BookingForm() {
     }
 
     return(
-        <form style={{display: "grid", maxWidth: "200px", gap: "20px"}} onSubmit={submitHandler}>
-            <label htmlFor="res_date">Date</label>
-            <input type="date" name="res_date" id="res_date" onChange={(e) => setDate(e.target.value)} value={date}/>
-            <label htmlFor="res_time">Time</label>
-            <select id="res_time" onChange={(e) => setTime(e.target.value)} value={time}>
-                {availableTimes.map((x) => <option>{x}</option>)}
-            </select>
-            <label htmlFor="guests">Number of Guests</label>
-            <input type="number" placeholder="1" name="guests" id="guests" min="1" max="10" onChange={e => setGuests(e.target.value)} value={guests}/>
-            <label htmlFor="occasion">Occasion</label>
-            <select id="occasion" onChange={e => setOccasion(e.target.value)} value={occasion}>
-                <option>Birthday</option>
-                <option>Anniversary</option>
-                <option>Other</option>
-            </select>
-            <input type="submit" value="Make your reservation" />
-        </form>
+        <>
+            <h1>Book Now</h1>
+            <form style={{display: "grid", maxWidth: "200px", gap: "20px"}} onSubmit={submitHandler}>
+                <label htmlFor="res_date" id="res_date">Date</label>
+                <input type="date" name="res_date" id="res_date" aria-labelledby="res_date" onChange={(e) => {setDate(e.target.value); props.dispatch(); console.log({availableTimes})}} value={date}/>
+                <label htmlFor="res_time" id="res_time">Time</label>
+                <select id="res_time" aria-labelledby="res_time" onChange={(e) => setTime(e.target.value)} value={time}>
+                    {availableTimes.map((x) => <option key={x}>{x}</option>)}
+                </select>
+                <label htmlFor="guests" id="guests">Number of Guests</label>
+                <input type="number" aria-labelledby="guests" placeholder="1" name="guests" id="guests" min="1" max="10" onChange={e => {setGuests(e.target.value)}} value={guests}/>
+                <label htmlFor="occasion" id="occasion">Occasion</label>
+                <select id="occasion" aria-labelledby="occasion" onChange={e => setOccasion(e.target.value)} value={occasion}>
+                    <option key="birthday">Birthday</option>
+                    <option key="anniversary">Anniversary</option>
+                    <option key="other_occasion">Other</option>
+                </select>
+                <input type="submit" value="Make your reservation" />
+            </form>
+        </>
     )
 }
 
