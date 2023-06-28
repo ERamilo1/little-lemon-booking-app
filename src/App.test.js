47c95c0da72de4a5577a24ded9a41251a5758119
Edwin Ramilo <ramilo.edwin.business@gmail.com>
2023-06-27 21:54:00 -0400

    Created unit tests for initializeTimes and updateTimes functions.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import BookingForm from './BookingForm';
+import { initializeTimes, updateTimes} from './App';
 
 test('Renders the BookingForm heading', () => {
   render(<BookingForm />);
   const headingElement = screen.getByText("Book Now");
   expect(headingElement).toBeInTheDocument();
 });
+
+test("initializeTimes function returns correct expected value", () => {
+  const testArray = ["a", "b", "c"];
+
+  expect(initializeTimes(testArray)).toStrictEqual(["a", "b", "c"]);
+})
+
+test("updateTimes function returns the same value that is provided in state", () => {
+  const testState = ["d", "e", "f"];
+
+  expect(updateTimes(testState, null)).toStrictEqual(["d", "e", "f"]);
+})
