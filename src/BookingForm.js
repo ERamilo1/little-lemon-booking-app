5a109a2d2a78f62ffce301fe22e7c38f7753c728
Edwin Ramilo <ramilo.edwin.business@gmail.com>
2023-08-29 17:48:13 -0400

    Updated validation for inputs. Disabled submit button until form is valid.

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -2,10 +2,10 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function BookingForm(props) {
-    const [time, setTime] = useState("");
-    const [date, setDate] = useState(new Date());
+    const [time, setTime] = useState("Select a time");
+    const [date, setDate] = useState("");
     const [guests, setGuests] = useState();
-    const [occasion, setOccasion] = useState("");
+    const [occasion, setOccasion] = useState("Select an occasion");
     const availableTimes = props.avTime;
     const formData = {date: date, time: time, guests: guests, occasion: occasion};
     const submitForm = props.onSubmit;
@@ -15,7 +15,7 @@ function BookingForm(props) {
     const day = today.getDate();
     const todaysDate = `${year}-${month}-${day}`;
     const getIsFormValid = () => {
-        return(date && time && 0 < guests < 11 && occasion);
+        return(date && time != "Select a time" && guests && occasion !== "Select an occasion" ? true : false);
     }
 
     return(
@@ -25,18 +25,20 @@ function BookingForm(props) {
                 <label htmlFor="res_date" id="res_date">Date</label>
                 <input type="date" name="res_date" id="res_date" aria-labelledby="res_date" onChange={(e) => {setDate(e.target.value); props.dispatch(new Date(e.target.value)); console.log({availableTimes})}} value={date} required min={todaysDate} />
                 <label htmlFor="res_time" id="res_time">Time</label>
-                <select id="res_time" aria-labelledby="res_time" onChange={(e) => setTime(e.target.value)} value={time}>
+                <select id="res_time" placeholder="Pick a time" aria-labelledby="res_time" onChange={(e) => setTime(e.target.value)} value={time}>
+                    <option key="default">Select a time</option>
                     {availableTimes?.map((x) => <option key={x}>{x}</option>)}
                 </select>
                 <label htmlFor="guests" id="guests">Number of Guests</label>
-                <input type="number" aria-labelledby="guests" placeholder="1" name="guests" id="guests" min="1" max="10" onChange={e => {setGuests(e.target.value)}} value={guests}/>
+                <input type="number" aria-labelledby="guests" placeholder="Number of Guests" name="guests" id="guests" min="1" max="10" onChange={e => {setGuests(e.target.value)}} value={guests}/>
                 <label htmlFor="occasion" id="occasion">Occasion</label>
                 <select id="occasion" aria-labelledby="occasion" onChange={e => setOccasion(e.target.value)} value={occasion}>
+                    <option key="default">Select an occasion</option>
                     <option key="birthday">Birthday</option>
                     <option key="anniversary">Anniversary</option>
                     <option key="other_occasion">Other</option>
                 </select>
-                <input type="submit" value="Make your reservation"  disabled={!getIsFormValid} />
+                <input type="submit" value="Make your reservation"  disabled={!getIsFormValid()} />
             </form>
         </>
     )
