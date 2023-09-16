c37399667e76d7df0b85c3250382060c58b1622b
Edwin Ramilo <ramilo.edwin.business@gmail.com>
2023-09-16 02:48:23 -0400

    Added more media rules for footer. Added id attributes to ul elements in Footer component.

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ function Footer(){
         <div className="footer">
             <footer>
                 <img src={logo} className="footer"/>
-                <ul className="footer">
+                <ul className="footer" id="footerNav">
                     <h4>Navigation</h4>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
@@ -15,13 +15,13 @@ function Footer(){
                     <li><Link to="/onlineOrder">Order Online</Link></li>
                     <li><Link to="/login">Login</Link></li>
                 </ul>
-                <ul className="footer">
+                <ul className="footer" id="footerContact">
                     <h4>Contact</h4>
                     <li>Address</li>
                     <li>Phone Number</li>
                     <li>e-mail</li>
                 </ul>
-                <ul className="footer">
+                <ul className="footer" id="footerSM">
                     <h4>Social Media</h4>
                     <li><a href="https://www.instagram.com/" target="_blank">Instagram</a></li>
                     <li><a href="https://www.facebook.com/" target="_blank">Facebook</a></li>
