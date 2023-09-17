e58955c70b58c7896bf49428ef61b2a16bc7f9a8
Edwin Ramilo <ramilo.edwin.business@gmail.com>
2023-09-17 17:07:41 -0400

    Added animation to header component, so that it translates up out of the window when scrolling down, but appears at the top of the window again when scrolling up

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,40 @@
 import Nav from "./Nav";
-import logo from "./images/Logo.svg"
+import logo from "./images/Logo.svg";
+import {useRef, useEffect} from "react"
 
 function Header(){
+    const headerRef = useRef(null);
+
+    useEffect(() => {
+        let prevScrollPos = window.scrollY;
+        const handleScroll = () => {
+            let currentScrollPos = window.scrollY;
+            const headerElement = headerRef.current;
+
+            if(!headerElement){
+                return;
+            }
+            if(prevScrollPos > currentScrollPos){
+                headerElement.style.transform = "translateY(0px)"
+            } else{
+                headerElement.style.transform = "translateY(-55px)"
+            }
+
+            prevScrollPos = currentScrollPos;
+
+            console.log("currentScrollPos: " + currentScrollPos);
+            console.log("prevScrollPos: " + prevScrollPos);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return() => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, [])
+
     return(
-        <div className="headerBG">
+        <div className="headerBG" ref={headerRef}>
             <div className="header">
                 <header>
                     <img src={logo} className="header"/>
