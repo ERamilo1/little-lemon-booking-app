ecb25b09746af81ec03074df005db7a78c3c419f
Edwin Ramilo <ramilo.edwin.business@gmail.com>
2023-09-17 20:56:07 -0400

    Created a dropdown menu for the navigation bar when viewport width is less than 720px.

--- src/Nav.js
+++ src/Nav.js
@@ -3,6 +3,21 @@ import { Link } from "react-router-dom";
 function Nav(){
     return(
         <nav className="nav">
+            <div className="dropDown">
+                <button className="dropDown">
+                    <div className="burgerTop"></div>
+                    <div className="burgerMid"></div>
+                    <div className="burgerBot"></div>
+                </button>
+                <div className="dropDownContent">
+                    <div className="dropDownContentItem"><Link to="/">Home</Link></div>
+                    <div className="dropDownContentItem"><Link to="/about">About</Link></div>
+                    <div className="dropDownContentItem"><Link to="/menu">Menu</Link></div>
+                    <div className="dropDownContentItem"><Link to="/reservations">Reservations</Link></div>
+                    <div className="dropDownContentItem"><Link to="/onlineOrder">Order Online</Link></div>
+                    <div className="dropDownContentItem"><Link to="/login">Login</Link></div>
+                </div>
+            </div>
             <ul className ="nav">
                 <li className="nav"><Link to="/">Home</Link></li>
                 <li className="nav"><Link to="/about">About</Link></li>
